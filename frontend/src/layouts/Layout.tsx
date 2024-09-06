78f500a151c3d8b7fa311a996f76a96dae6fbc74
Wilord20 <edgartorres08@live.com.mx>
2024-09-06 14:22:28 -0600

    Setting Up Footer

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header"
 import Hero from "../components/Hero";
 
@@ -5,6 +6,7 @@ const Layout = () => {
     return <div className="flex flex-col min-h-screen">
         <Header />
         < Hero />
+        < Footer />
     </div>
 }
 
