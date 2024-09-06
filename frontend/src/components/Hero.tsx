3574c6afbedf7bbf850fe873db01609121b9b817
Wilord20 <edgartorres08@live.com.mx>
2024-09-05 19:07:25 -0600

    Hero component

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+    return (
+        <div className="bg-blue-800 pb-16">
+            <div className="container mx-auto flex flex-col gap-2">
+                <h1 className="text-5xl text-white font-bold">
+                    Encuentre la estancia perfecta
+                </h1>
+                <p className="text-2xl text-white">
+                    Los mejores precios para el hotel de sus sueños
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default Hero;
