e44f9d2a0ef3646d10a117a5e1cae03f490b7f41
Wilord20 <edgartorres08@live.com.mx>
2024-09-06 15:11:26 -0600

    Adding packages for WJT and Crypting, also implementing them

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,21 +1,35 @@
 import express, { Request, Response } from "express";
 import User from "../models/user";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/register", async (req: Request, res: Response) => {
   try {
     let user = await User.findOne({
-        email: req.body.email
+      email: req.body.email,
     });
 
     if (user) {
-        return res.status(400).json({ message: "El usuario ya existe" });
+      return res.status(400).json({ message: "El usuario ya existe" });
     }
 
     user = new User(req.body);
     await user.save();
 
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      { expiresIn: "1d" }
+    );
+
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+
+    return res.sendStatus(200);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Algo salió mal" });
