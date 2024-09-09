6a1c4c0401bc9420b6df69f562eb4592ab2bd84f
Wilord20 <edgartorres08@live.com.mx>
2024-09-09 14:24:09 -0600

    Register page created

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,25 +1,111 @@
 import { useForm } from "react-hook-form";
 
 type RegisterFormData = {
-    email: string;
-    password: string;
-    confirmPassword: string;
-    firstName: string;
-    lastName: string;
+  // Define los campos del formulario
+  email: string;
+  password: string;
+  confirmPassword: string;
+  firstName: string;
+  lastName: string;
 };
 
-
 const Register = () => {
+  const {
+    register,
+    watch, // Función para observar los cambios en un campo
+    handleSubmit, // Función para manejar el envío del formulario
+    formState: { errors }, // Extraer los errores del hook de useForm
+  } = useForm<RegisterFormData>(); // Inicializar el hook de useForm con el tipo de datos de la forma
 
-    const { register } = useForm<RegisterFormData>();
+  const onSubmit = handleSubmit((data) => {
+    // Función que se ejecutará al enviar el formulario
+    console.log(data);
+  });
 
-    return (
-        <form className="flex flex-col gap-5">
-            <h2 className="text-3xl font-bold">
-                Crea una cuenta
-            </h2>
-        </form>
-    );
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="text-3xl font-bold">Crea una cuenta</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Nombre(s)
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("firstName", { required: "El campo es necesario" })}
+          ></input>
+          {errors.firstName && ( // Si hay un error en el campo
+            <span className="text-red-500">{errors.firstName.message}</span>
+          )}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Apellidos
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("lastName", { required: "El campo es necesario" })}
+          ></input>
+          {errors.lastName && ( // Si hay un error en el campo
+            <span className="text-red-500">{errors.lastName.message}</span>
+          )}
+        </label>
+      </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Correo electrónico
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "El campo es necesario" })}
+        ></input>
+        {errors.email && ( // Si hay un error en el campo
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Contraseña
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "El campo es necesario",
+            minLength: {
+              value: 6,
+              message: "La contraseña debe tener al menos 6 carácteres",
+            },
+          })}
+        ></input>
+        {errors.password && ( // Si hay un error en el campo
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirmar Contraseña
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmPassword", {
+            validate: (val) => {
+              if (!val) {
+                // Si el campo está vacío
+                return "El campo es necesario";
+              } else if (watch("password") !== val) {
+                // Si las contraseñas no coinciden
+                return "Las contraseñas no coinciden";
+              }
+            },
+          })}
+        ></input>
+        {errors.confirmPassword && ( // Si hay un error en el campo
+          <span className="text-red-500">{errors.confirmPassword.message}</span>
+        )}
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+        >
+          Registrarse
+        </button>
+      </span>
+    </form>
+  );
 };
 
 export default Register;
