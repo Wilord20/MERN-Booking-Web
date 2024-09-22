122dc17f64acdd08d2f367dd9f61f783254fff7a
Wilord20 <edgartorres08@live.com.mx>
2024-09-21 18:34:59 -0600

    Refactor SearchContext for better organization and add SearchContextProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { AppContextProvider } from './contexts/AppContext.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContextProvider } from "./contexts/AppContext.tsx";
+import { SearchContextProvider } from "./contexts/SearchContext.tsx";
 
-const queryClient = new QueryClient({ // Crear una instancia de QueryClient
+const queryClient = new QueryClient({
+  // Crear una instancia de QueryClient
   defaultOptions: {
     queries: {
       retry: 0, // Deshabilitar la reintentos de las consultas
     },
   },
-})
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
-        <App />
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
       </AppContextProvider>
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
