9d7691f90aed02c91310a8aef4aa19c8b32a0af9
Wilord20 <edgartorres08@live.com.mx>
2024-10-02 20:13:20 -0600

    Test file for booking

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -30,7 +30,9 @@ test("Debería mostrar hoteles encontrados", async ({ page }) => {
   await page.getByPlaceholder("¿A dónde te diriges?").fill("Test País");
   await page.getByRole("button", { name: "Buscar" }).click();
 
-  await expect(page.getByText("Hoteles encontrados en Test País")).toBeVisible();
+  await expect(
+    page.getByText("Hoteles encontrados en Test País")
+  ).toBeVisible();
   await expect(page.getByText("Test Hotel").first()).toBeVisible();
 });
 
@@ -42,6 +44,35 @@ test("Debería mostrar detalles de un hotel", async ({ page }) => {
 
   await page.getByText("Test Hotel").first().click();
   await expect(page).toHaveURL(/detail/);
-  await expect(page.getByRole("button", { name: "Reserva ahora" })).toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "Reserva ahora" })
+  ).toBeVisible();
 });
 
+test("Debería reservar un hotel", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("¿A dónde te diriges?").fill("Test País");
+  const date = new Date();
+  date.setDate(date.getDate() + 3); // Fecha de salida en 3 días
+  const formattedDate = date.toISOString().split("T")[0]; // Formato de fecha YYYY-MM-DD
+  await page.getByPlaceholder("Check-out Date").fill(formattedDate);
+
+  await page.getByRole("button", { name: "Buscar" }).click();
+
+  await page.getByText("Test Hotel").first().click();
+  await page.getByRole("button", { name: "Reserva ahora" }).click();
+
+  await expect(page.getByText("Costo total: MXN 3000.00")).toBeVisible();
+
+  const stripeFrame = page.frameLocator("iframe").first();
+  await stripeFrame
+    .locator("[placeholder='Card number']")
+    .fill("4242424242424242");
+  await stripeFrame.locator("[placeholder='MM / YY']").fill("12/40");
+  await stripeFrame.locator("[placeholder='CVC']").fill("123");
+  await stripeFrame.locator("[placeholder='ZIP']").fill("12345");
+
+  await page.getByRole("button", { name: "Confirmar Reservación" }).click();
+  await expect(page.getByText("Reservación exitosa")).toBeVisible();
+});
